31e12acf34110800676af6f6d79abe8def173d78
Rai Phairow <rai.phairow@github.com>
2017-03-20 17:56:21 -0700

    finally

--- bin/reload-varnishd.sh
+++ bin/reload-varnishd.sh
@@ -3,8 +3,14 @@
 # Generate a unique timestamp ID for this version of the VCL
 TIME=$(date +%s)
 
+CURRENT_CONFIG=$( $VARNISHADM vcl.list | awk ' /^active/ { print $3 } ' )
+
 # Load the file into memory
 varnishadm -S /etc/varnish/secret -T 127.0.0.1:6082 vcl.load varnish_$TIME /etc/varnish/default.vcl
 
 # Active this Varnish config
 varnishadm -S /etc/varnish/secret -T 127.0.0.1:6082 vcl.use varnish_$TIME
+
+varnishadm -S /etc/varnish/secret -T 127.0.0.1:6082 vcl.discard $CURRENT_CONFIG
+
+exit 0
--- bin/start-varnishd.sh
+++ bin/start-varnishd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec varnishd -F -f /etc/varnish/default.vcl -s malloc,${VARNISH_MEMORY} -a 0.0.0.0:${VARNISH_PORT} -T 127.0.0.1:6082 -p http_req_hdr_len=16384 -p http_resp_hdr_len=16384
+exec varnishd -F -f /etc/varnish/default.vcl -s malloc,${VARNISH_MEMORY} -a 0.0.0.0:${VARNISH_PORT} -T 127.0.0.1:6082 -S /etc/varnish/secret -p http_req_hdr_len=16384 -p http_resp_hdr_len=16384
